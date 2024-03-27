23ab5f865bca3eb6f8083d0a449fdc8428d20fc1
allasca <allasca@users.noreply.github.com>
2024-03-27 11:08:54 +0000

    deploy: 3be1a69eb05b530cb99a8970c1df5037586b41b8

--- js/third-party/addtoany.js
+++
@@ -1,8 +0,0 @@
-/* global NexT */
-
-document.addEventListener('page:loaded', () => {
-  NexT.utils.getScript('https://static.addtoany.com/menu/page.js', { condition: window.a2a })
-    .then(() => {
-      window.a2a.init();
-    });
-});
